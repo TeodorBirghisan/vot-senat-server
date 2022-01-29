b067dc25afa3d32fcd6258a3af00cf7a0c790d33
Teodor Birghisan <birghisanteodor@yahoo.com>
2022-01-29 16:56:02 +0200

    Implemented exit and get all users in meeting

--- src/modules/participation-entry/participation-entry.controller.ts
+++ src/modules/participation-entry/participation-entry.controller.ts
@@ -1,12 +1,15 @@
 import { ParticipationEntryService } from './participation-entry.service';
 import {
   Controller,
+  Get,
   HttpStatus,
   Param,
   ParseIntPipe,
   Post,
+  Put,
 } from '@nestjs/common';
 import { ParticipationEntry } from './partitcipation-entry.entity';
+import { User } from '../user/user.entity';
 
 @Controller('/participation')
 export class ParticipationEntryController {
@@ -26,5 +29,32 @@ export class ParticipationEntryController {
     userId: number,
   ): Promise<ParticipationEntry> {
     return this.participationEntryService.joinMeeting(meetingId, userId);
+  }
+
+  @Put('/exitMeeting/:meetingId/:userId')
+  exitMeeting(
+    @Param(
+      'meetingId',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    meetingId: number,
+    @Param(
+      'userId',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    userId: number,
+  ): Promise<ParticipationEntry> {
+    return this.participationEntryService.exitMeeting(meetingId, userId);
+  }
+
+  @Get('/allUsers/:meetingId')
+  getAllUsersInMeeting(
+    @Param(
+      'meetingId',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    meetingId: number,
+  ): Promise<User[]> {
+    return this.participationEntryService.getAllUsersInMeeting(meetingId);
   }
 }
--- src/modules/participation-entry/participation-entry.service.ts
+++ src/modules/participation-entry/participation-entry.service.ts
@@ -1,8 +1,12 @@
+import {
+  MEETING_STATUS_FINISHED,
+  MEETING_STATUS_TO_BE_DISSCUSSED,
+} from './../../core/constants/index';
 import { MeetingService } from './../meeting/meeting.service';
 import { UserService } from './../user/user.service';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { IsNull, Repository } from 'typeorm';
 import { ParticipationEntry } from './partitcipation-entry.entity';
 import { User } from '../user/user.entity';
 import { MEETING_STATUS_IN_PROGRESS } from 'src/core/constants';
@@ -27,6 +31,13 @@ export class ParticipationEntryService {
         user: userId,
       },
     });
+  }
+
+  async countAllUsersInMeeting(meetingId: number): Promise<number> {
+    const usersCount: number = await this.participationEntryRepository.count({
+      where: { meeting: meetingId, exitTimestamp: IsNull() },
+    });
+    return usersCount;
   }
 
   async joinMeeting(
@@ -57,8 +68,10 @@ export class ParticipationEntryService {
         );
       }
 
-      //TODO change status of meeting
-      meetingToJoin.status = MEETING_STATUS_IN_PROGRESS;
+      await this.meetingService.udpateStatus(
+        meetingId,
+        MEETING_STATUS_IN_PROGRESS,
+      );
       const participationEntry: ParticipationEntry =
         this.participationEntryRepository.create({
           joinTimestamp: timeToJoin,
@@ -67,6 +80,67 @@ export class ParticipationEntryService {
           exitTimestamp: null,
         });
       return await this.participationEntryRepository.save(participationEntry);
+    }
+  }
+
+  async exitMeeting(
+    meetingId: number,
+    userId: number,
+  ): Promise<ParticipationEntry> {
+    const timeToExit: Date = new Date();
+
+    const participationEntry: ParticipationEntry =
+      await this.findOneByMeetingAndUser(meetingId, userId);
+
+    if (!participationEntry) {
+      throw new HttpException(
+        'Cannot exit a meeting that you did not join',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    participationEntry.exitTimestamp = timeToExit;
+
+    const usersCount: number = await this.countAllUsersInMeeting(meetingId);
+
+    if (usersCount === 1) {
+      await this.meetingService.udpateStatus(
+        meetingId,
+        MEETING_STATUS_FINISHED,
+      );
+    }
+
+    return this.participationEntryRepository.save(participationEntry);
+  }
+
+  async getAllUsersInMeeting(meetingId: number): Promise<User[]> {
+    const meeting: Meeting = await this.meetingService.findOneById(meetingId);
+
+    if (meeting.status == MEETING_STATUS_TO_BE_DISSCUSSED) {
+      throw new HttpException(
+        'This meeting has not started',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    if (meeting.status == MEETING_STATUS_IN_PROGRESS) {
+      const participations: ParticipationEntry[] =
+        await this.participationEntryRepository.find({
+          where: { meeting: meetingId, exitTimestamp: IsNull() },
+          relations: ['user'],
+        });
+      const users: User[] = participations.map(({ user }) => user);
+      return users;
+    }
+
+    if (meeting.status == MEETING_STATUS_FINISHED) {
+      const participations: ParticipationEntry[] =
+        await this.participationEntryRepository.find({
+          where: { meeting: meetingId },
+          relations: ['user'],
+        });
+      const users: User[] = participations.map(({ user }) => user);
+      return users;
     }
   }
 }
