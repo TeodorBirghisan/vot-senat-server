c80598dee697eec4173cd3834c09ef2b8fb6c46d
Teodor Birghisan <birghisanteodor@yahoo.com>
2022-02-24 15:47:01 +0200

    Implemented organizer to create and delete own meetings

--- src/modules/auth-jwt/auth.controller.ts
+++ src/modules/auth-jwt/auth.controller.ts
@@ -6,20 +6,55 @@ import {
   UseGuards,
   Request,
   Body,
+  Req,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { CreateUserDto, LoginUserDto } from '../user/user.dto';
 import { AuthJwtService } from './auth-jwt.service';
+import { InvitationGuard } from '../invitation/invitation.guard';
+import { InvitationService } from '../invitation/invitation.service';
 
 @Controller('/auth-jwt')
 export class AuthJWTController {
-  constructor(private readonly authService: AuthJwtService) {}
+  constructor(
+    private readonly authService: AuthJwtService,
+    private readonly invitationService: InvitationService,
+  ) {}
 
   @Post('register')
   async register(@Body() createUserDto: CreateUserDto): Promise<any> {
     const result = await this.authService.register(createUserDto);
     return result;
   }
+
+  // @Post('invitation/register')
+  // @UseGuards(InvitationGuard)
+  // async registerWithInvitation(
+  //   @Req() request: Request,
+  //   @Body() createUserDto: CreateUserDto,
+  // ): Promise<any> {
+  //   const invitationToken: string = request.headers['authorization'];
+
+  //   const result = await this.authService.register(createUserDto);
+
+  //   //TBD instead of deletion, the invitation should be invalidated, instead of deleted
+  //   const deleteionSuccessfull =
+  //     await this.invitationService.deleteInvitationByToken(invitationToken);
+
+  //   if (!deleteionSuccessfull) {
+  //     //TODO rollback user creation
+  //     throw new HttpException(
+  //       'Error deleting deleting the invitation',
+  //       HttpStatus.CONFLICT,
+  //     );
+  //   }
+
+  //   return {
+  //     message: 'User created successfully',
+  //   };
+  // }
 
   @Post('login')
   public async login(@Body() loginUserDto: LoginUserDto): Promise<any> {
--- src/modules/meeting/meeting.controller.ts
+++ src/modules/meeting/meeting.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   ParseIntPipe,
   Post,
+  Req,
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
@@ -25,21 +26,12 @@ export class MeetingsController {
     return this.meetingService.getAll();
   }
 
-  //TODO: Organizer = user making the req. For now userId will replace that
-  @Post('/:userId')
+  @Post()
   createMeeting(
-    @Param(
-      'userId',
-      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
-    )
-    userId: number,
     @Body() meeting: MeetingDTO,
+    @Req() req: any,
   ): Promise<Meeting> {
-    if (!meeting.description) {
-      meeting.description = 'No description provided';
-    }
-
-    return this.meetingService.saveOne(userId, meeting);
+    return this.meetingService.saveOne(req, meeting);
   }
 
   @Delete('/:meetingId')
@@ -49,7 +41,8 @@ export class MeetingsController {
       new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
     )
     meetingId: number,
+    @Req() req: any,
   ): Promise<Meeting> {
-    return this.meetingService.deleteOne(meetingId);
+    return this.meetingService.deleteOne(req, meetingId);
   }
 }
