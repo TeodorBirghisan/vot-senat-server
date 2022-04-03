00a2cb03694388fc023ef13903974ff1e8e3970c
Teodor Birghisan <birghisanteodor@yahoo.com>
2022-04-03 17:29:40 +0300

    renamed const name

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -3,7 +3,7 @@ export const DEVELOPMENT = 'development';
 export const TEST = 'test';
 export const PRODUCTION = 'production';
 
-export const MEETING_STATUS_IN_PROGRESS = 'IN PROGRESS';
+export const MEETING_STATUS_IN_PROGRESS = 'IN_PROGRESS';
 export const MEETING_STATUS_FINISHED = 'FINISHED';
 export const MEETING_STATUS_TO_BE_DISSCUSSED = 'TO_BE_DISSCUSSED';
 
