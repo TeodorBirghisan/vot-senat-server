cb587dc2165f6cd8190bdc1f83185eab2904da7f
Teodor Birghisan <birghisanteodor@yahoo.com>
2022-06-30 20:36:16 +0300

    fixed send email

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -2,10 +2,19 @@ import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { google } from 'googleapis';
 
 import * as dotenv from 'dotenv';
 
 dotenv.config();
+
+const OAuth2 = google.auth.OAuth2;
+
+const ouath2Client = new OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  'https://developers.google.com/oauthplayground',
+);
 
 @Module({
   imports: [
@@ -21,6 +30,7 @@ dotenv.config();
           clientId: process.env.CLIENT_ID,
           clientSecret: process.env.CLIENT_SECRET,
           refreshToken: process.env.REFRESH_TOKEN,
+          accessToken: async () => await ouath2Client.getAccessToken(),
           tls: {
             rejectUnauthorized: false,
           },
